bd5110fdce27abfda68d0282f2cc4f56df148556
WarriorsSami <sbarbutdica@gmail.com>
2021-11-04 17:25:16 +0200

    feat(cors): enable cors origins

--- src/main/kotlin/com/sami/jwtauthapi/WebConfig.kt
+++ src/main/kotlin/com/sami/jwtauthapi/WebConfig.kt
@@ -1,11 +1,17 @@
 package com.sami.jwtauthapi
 
+import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-class WebCofig: WebMvcConfigurer {
+@Configuration
+@EnableWebMvc
+class WebConfig: WebMvcConfigurer {
 
-    public override fun addCorsMappings(registry: CorsRegistry) {
-        
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://localhost:4200")
+            .allowCredentials(true)
     }
 }
